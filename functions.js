668a327fce5ce3075f802530091dafe24a18afb4
BenjaminCline17 <duckmancline@gmail.com>
2024-09-17 15:11:22 -0500

    completed remove function

--- functions.js
+++ functions.js
@@ -13,6 +13,11 @@
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+//I:Function takes in an input array and a input string
+//O:Function should look through the input array to see if the input string exists in it, returning the array's object if so and null if not
+//C:N/A
+//E:N/A
+
 function search(array, string){
     //for loop to iterate through array input
     for (let i = 0; i < array.length; i++){
@@ -27,13 +32,17 @@ function search(array, string){
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//I:Function takes in an animals array, a name string, and a replacement object
+//O:Function should replace the animal array's entire object with the replacement object if the name string exists within the animals array
+//C:N/A
+//E:N/A
 
-function replace(array, name, replacement){
+function replace(animals, name, replacement){
     //use a for loop to iterate through the input array
-    for (let i = 0; i < array.length; i++){
+    for (let i = 0; i < animals.length; i++){
         //if the current index of the input array's name key equals input name, change the array's entire object with the input replacement
-        if (array[i].name == name){
-            array[i] = replacement;
+        if (animals[i].name == name){
+            animals[i] = replacement;
         }
     }
 }
@@ -42,8 +51,20 @@ function replace(array, name, replacement){
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//I:Function takes in an animal array and a name string
+//O:Function should remove an animal with the name string
+//C:N/A
+//E:N/A
 
-
+function remove(animals, name){
+ //use a for loop to iterate through the input array
+ for (let i = 0; i < animals.length; i++){
+    //if the name at the current animals index equals the input name, remove the animals array
+    if(animals[i].name == name){
+       return animals.splice(i);
+    }
+ }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
